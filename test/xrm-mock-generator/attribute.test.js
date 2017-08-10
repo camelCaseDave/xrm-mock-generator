80ec43043b546a79f70c3ffdda151616721f44dd
David Clark <David.Clark@chorus.co>
2017-08-10 09:29:03 +0100

    updated npm publish. added some more tests

--- test/xrm-mock-generator/attribute.test.js
+++ test/xrm-mock-generator/attribute.test.js
@@ -2,7 +2,28 @@ describe("XrmMockGenerator.Attribute", function () {
     var XrmMock = require("xrm-mock");
     var XrmMockGenerator = require("../../src/xrm-mock-generator.js");
 
+    beforeEach(function () {
+        XrmMockGenerator.initialise();
+    });
+
     it("should exist", function () {
         expect(XrmMockGenerator.Attribute).toBeDefined();
-    });    
+    });
+
+    it("should create a bool", function () {
+        XrmMockGenerator.Attribute.createBool("new_havingfun", true);
+        expect(Xrm.Page.getAttribute("new_havingfun").getValue()).toBe(true);
+    });
+
+    it("should create a date", function () {
+        var date = new Date(2017, 1, 1);
+        XrmMockGenerator.Attribute.createDate("birthdate", date);
+        expect(Xrm.Page.getAttribute("birthdate").getValue()).toBe(date);
+    });
+
+    it("should create a datetime", function () {
+        var date = new Date(2017, 1, 1);
+        XrmMockGenerator.Attribute.createDateTime("birthdate", date);
+        expect(Xrm.Page.getAttribute("birthdate").getValue()).toBe(date);
+    });
 });
